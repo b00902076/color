d2ebefa01880cde0d8b63d69ae71376c4d3deb49
huang <huang@nihon-ma.co.jp>
2017-11-08 17:45:43 +0900

    Remove SQL part

--- game.php
+++ game.php
@@ -53,61 +53,61 @@
 <!--     </div> -->
     
 
-    
-    <?php 
-        echo "<table class=\"board page\">";
-        echo "<tr class=\"board_top\">"."<td colspan=2>ランキング</td>";
-        echo "</tr>";
-        echo "<tr class=\"board_ctop\">";
-        echo "<td>Name</td><td>Score</td>";        
-        echo "</tr>";
-        $servername = "localhost";
-        $username = "root";
-        $password = "ppp";
-        $dbname = "huang";
-        //echo "Get name: ".$name."<br>Get score: ".$score."<br>";
-        
-        
-        // Create connection
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        
-        // Check connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }//echo "Connected successfully<br>";
-        
-        if( ($_POST["name"] != "" && $_POST["score"] != "") //&&
-            //($_SESSION["via_button"] == $_POST["via_button"]) &&
-            //($_SESSION["via_button"] == "unset")){
-            ){
-            $sql = "select max(uid) as maxuid from score_ranking";
-            $result = $conn->query($sql)->fetch_assoc();
-            //echo "maxuid= ".$result['maxuid']+1;
-            if($name == "") $name = "NoName";
-            $sql = "insert into score_ranking (uid, name, score) value ('".($result['maxuid']+1)."','".$_POST['name']."','".$_POST['score']."')";
-            $conn->query($sql);
-            $conn->close();
-            header('Location: http://127.0.0.1:8888/WebGame/game.php');
-            //$_POST = [];
-            //$_SESSION["via_button"]="submitted";
-        }
-        
-        $sql = "select * from score_ranking order by score desc, uid asc limit 20";
-        //$result = $conn->query($sql);
-        $result = $conn->query($sql);
-        
-        if ($result->num_rows > 0) {
-            // output data of each row
-            while($row = $result->fetch_array()) {
-                echo "<tr>";
-                echo "<td>{$row[1]}</td><td>{$row[2]}</td>";
-                echo "</tr>";
-            }
-        } else {
-            echo "<tr><td>0 results</tr></td>";
-        }
-        $conn->close();
-        echo "</table>";
+
+    <?php
+//        echo "<table class=\"board page\">";
+//        echo "<tr class=\"board_top\">"."<td colspan=2>ランキング</td>";
+//        echo "</tr>";
+//        echo "<tr class=\"board_ctop\">";
+//        echo "<td>Name</td><td>Score</td>";
+//        echo "</tr>";
+//        $servername = "localhost";
+//        $username = "root";
+//        $password = "ppp";
+//        $dbname = "huang";
+//        //echo "Get name: ".$name."<br>Get score: ".$score."<br>";
+//
+//
+//        // Create connection
+//        $conn = new mysqli($servername, $username, $password, $dbname);
+//
+//        // Check connection
+//        if ($conn->connect_error) {
+//            die("Connection failed: " . $conn->connect_error);
+//        }//echo "Connected successfully<br>";
+//
+//        if( ($_POST["name"] != "" && $_POST["score"] != "") //&&
+//            //($_SESSION["via_button"] == $_POST["via_button"]) &&
+//            //($_SESSION["via_button"] == "unset")){
+//            ){
+//            $sql = "select max(uid) as maxuid from score_ranking";
+//            $result = $conn->query($sql)->fetch_assoc();
+//            //echo "maxuid= ".$result['maxuid']+1;
+//            if($name == "") $name = "NoName";
+//            $sql = "insert into score_ranking (uid, name, score) value ('".($result['maxuid']+1)."','".$_POST['name']."','".$_POST['score']."')";
+//            $conn->query($sql);
+//            $conn->close();
+//            header('Location: http://127.0.0.1:8888/WebGame/game.php');
+//            //$_POST = [];
+//            //$_SESSION["via_button"]="submitted";
+//        }
+//
+//        $sql = "select * from score_ranking order by score desc, uid asc limit 20";
+//        //$result = $conn->query($sql);
+//        $result = $conn->query($sql);
+//
+//        if ($result->num_rows > 0) {
+//            // output data of each row
+//            while($row = $result->fetch_array()) {
+//                echo "<tr>";
+//                echo "<td>{$row[1]}</td><td>{$row[2]}</td>";
+//                echo "</tr>";
+//            }
+//        } else {
+//            echo "<tr><td>0 results</tr></td>";
+//        }
+//        $conn->close();
+//        echo "</table>";
     ?>
     <?php 
         //echo "\$_SESSION[\"via_button\"]= ".$_SESSION["via_button"]."<br>";
